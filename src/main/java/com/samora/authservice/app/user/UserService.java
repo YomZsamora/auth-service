eec43c64ba598cef4ef5c4feb2f4545ba7f5f004
yomZsamora <samaurah@gmail.com>
2026-05-23 09:18:45 +0300

    Add documentation for registerUser method in UserService

--- src/main/java/com/samora/authservice/app/user/UserService.java
+++ src/main/java/com/samora/authservice/app/user/UserService.java
@@ -1,4 +1,3 @@
-
 package com.samora.authservice.app.user;
 
 import com.samora.authservice.app.user.dto.RegisterUserRequest;
@@ -6,6 +5,7 @@ import com.samora.authservice.app.user.dto.UserResponse;
 
 public interface UserService {
 
+    // Registers a new user based on the provided registration request and returns the created user's details.
     UserResponse registerUser(RegisterUserRequest request);
 
 }
